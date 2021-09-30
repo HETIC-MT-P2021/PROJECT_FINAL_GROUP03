19b6efa6133aa3b5727429896b9088412951b818
edwinvautier <edwin.vautier@hetic.net>
2021-09-30 16:36:29 +0200

    Refacto: lint code

--- back/shared/repositories/accounts.go
+++ back/shared/repositories/accounts.go
@@ -21,7 +21,7 @@ func UpdateAccount(account *models.Account) error {
 
 func FindAccountServers(account *models.Account) ([]models.Server, error) {
 	var servers []models.Server
-	err := database.Db.Debug().Model(&account).Association("Servers").Find(&servers).Error
+	err := database.Db.Debug().Model(&account).Association("Servers").Find(&servers)
 
 	return servers, err
 }
