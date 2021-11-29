d76c4113aafbbe7a564924c20f941e648f049de0
edwinvautier <edwin.vautier@hetic.net>
2021-11-29 23:07:33 +0100

    [feat] login and help commands

--- bot/cmd/login_command.go
+++ bot/cmd/login_command.go
@@ -1,1 +1,23 @@
 package cmd
+
+import (
+	"errors"
+	"github.com/JackMaarek/go-bot-utils/env"
+)
+
+type LoginCommand struct {
+	gc *GenericCommand
+}
+
+func (command LoginCommand) Execute() error {
+	var frontUrl string
+	if frontUrl = env.GetVariable("FRONT_URL"); frontUrl == "" {
+		return errors.New("MISSING ENV VARIABLE FRONT_URL")
+	}
+
+	if _, err := command.gc.Session.ChannelMessageSend(command.gc.Message.ChannelID, frontUrl); err != nil {
+		return err
+	}
+
+	return nil
+}
